9c2d55fbfe22280e02b88dec36bb7a67b7ab2f44
Yashita16 <yashitagupta427@gmail.com>
2026-03-23 22:03:23 +0530

    feat: add Navbar component and integrate it into Home page

---
+++ src/assets/assets.js
@@ -0,0 +1,8 @@
+import favicon from './favicon.png';
+
+const assets ={
+  logo: favicon
+}
+
+
+export default assets;
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const  AppContext = createContext();
+
+
+
+export const AppContextProvider = ({ children }) => {
+  const [user , setUser ] =useState(true);
+  const navigate = useNavigate();
+  const value = { user, setUser, navigate };
+
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+
+    </AppContext.Provider>
+  )
+
+}
+
+export const useAppContext = () => {
+  const context =useContext(AppContext)
+  if(!context){
+    throw new Error("useAppContext must be used inside AppContextProvider")
+  }
+  return context;
+}
+
